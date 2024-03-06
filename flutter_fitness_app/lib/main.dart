18680e0f6211b30360b323f6ff2ed3e5be056f5e
Jordan <colledgejordan@gmail.com>
2024-03-06 02:57:56 +0000

    Basic login screen functionality

--- flutter_fitness_app/lib/main.dart
+++ flutter_fitness_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'theme_provider.dart'; // Import the theme_provider.dart file
 import 'home.dart'; //Import the home file
 
@@ -12,6 +14,13 @@ void main() {
   );
 }
 
+class User {
+  final String username;
+  final String password;
+
+  User({required this.username, required this.password});
+}
+
 class FitnessApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -21,7 +30,74 @@ class FitnessApp extends StatelessWidget {
   }
 }
 
-class FitnessIntroPage extends StatelessWidget {
+class FitnessIntroPage extends StatefulWidget {
+  @override
+  _FitnessIntroPageState createState() => _FitnessIntroPageState();
+}
+
+class _FitnessIntroPageState extends State<FitnessIntroPage> {
+  late SharedPreferences _prefs;
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _initPrefs();
+  }
+
+  Future<void> _initPrefs() async {
+    _prefs = await SharedPreferences.getInstance();
+  }
+
+  void _login() {
+    final String username = _usernameController.text.trim();
+    final String password = _passwordController.text.trim();
+
+    final String? storedPassword = _prefs.getString(username);
+
+    if (storedPassword != null && storedPassword == password) {
+      // Navigate to the homepage if login successful
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => Homepage()),
+      );
+    } else {
+      // Prompt the user to sign up if login fails
+      _showSignupDialog();
+    }
+  }
+
+  void _showSignupDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Sign Up'),
+          content: Text('Create a new account?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                final String username = _usernameController.text.trim();
+                final String password = _passwordController.text.trim();
+                _prefs.setString(username, password);
+                Navigator.of(context).pop();
+                _login(); // Login after signing up
+              },
+              child: Text('Sign Up'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,16 +120,34 @@ class FitnessIntroPage extends StatelessWidget {
                 textAlign: TextAlign.center,
               ),
               SizedBox(height: 20.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: TextField(
+                  controller: _usernameController,
+                  decoration: InputDecoration(
+                    hintText: 'Username',
+                    fillColor: Colors.white,
+                    filled: true,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: TextField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    fillColor: Colors.white,
+                    filled: true,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
               ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => Homepage(),
-                    ),
-                  );
-                },
-                child: Text("Next"),
+                onPressed: _login,
+                child: Text("Login"),
               ),
             ],
           ),
